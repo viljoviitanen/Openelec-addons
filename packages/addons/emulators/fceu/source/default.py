157598dc973e6938bde600fa8246e3eec7570f15
Viljo Viitanen <viljo.viitanen@gmail.com>
2012-02-04 16:38:37 +0200

    Add command line parameters.. forgot these files

--- packages/addons/emulators/fceu/source/default.py
+++ packages/addons/emulators/fceu/source/default.py
@@ -30,8 +30,9 @@ fn = dialog.browse(1, 'FCEU', 'files')
 
 __settings__   = xbmcaddon.Addon(id='emulator.fceu')
 __cwd__        = __settings__.getAddonInfo('path')
+__param__        = __settings__.getSetting('param')
 __path__       = xbmc.translatePath( os.path.join( __cwd__, 'bin', "fceux") )
 
 os.system( "chmod a+rx " + __path__ )
-os.system( "%s --fullscreen 1 --opengl 1 '%s' "%(__path__,fn) )
+os.system( "%s %s '%s' "%(__path__,__param__,fn.replace("'", "'\\''")) )
 
